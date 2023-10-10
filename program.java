ef56fd1ef4c6c9e618386c0ce7092e29a7dbafa1
Divyanshu422 <divyanshu422@gmail.com>
2023-10-10 20:36:31 +0530

    Java program

---
+++ program.java
@@ -0,0 +1,18 @@
+//! String -> datatype?
+
+// class program {
+//     public static void main(String[] args) {
+//         String s = "Hello world";
+       
+//         //*  instanceOf operator
+//         if (s instanceof String) {
+//             System.out.println("str is an instance of String.");
+//         } else {
+//             System.out.println("str is not an instance of String.");
+//         }
+//     }
+// }
+
+//?-------------------------------------------------------------------
+
+
